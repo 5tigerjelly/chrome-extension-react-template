1ef666f122ac06c54ff2eba9d8d8024c4c1b58bd
Chris Oh <chris@5tigerjelly.com>
2024-07-06 22:22:26 -0400

    Add viteStaticCopy and update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/manifest.json',
+          dest: '.',
+        }
+      ],
+    }),
+  ],
+  build: {
+    outDir: 'build',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    },
+  },
+});
